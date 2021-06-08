c845318abd78c443a1c3f7f245668c133779784f
gh-ooooseok <62334707+gh-ooooseok@users.noreply.github.com>
2021-06-09 01:41:21 +0900

    !O firebase setting

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+apiKey: "AIzaSyBct9fMwH0toT14a0ZHa42CBwRPdfvMwo8",
+authDomain: "designproject1-f2b81.firebaseapp.com",
+projectId: "designproject1-f2b81",
+storageBucket: "designproject1-f2b81.appspot.com",
+messagingSenderId: "1083905265252",
+appId: "1:1083905265252:web:c6aabfda1174daf0ff5971",
+measurementId: "G-NDJGR7FKDK"
+};
+
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
