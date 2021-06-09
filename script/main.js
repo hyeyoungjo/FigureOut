c2b93601df0eba731edd8a8c85d561ac2caf2c10
ykim18 <ykim18@kaist.ac.kr>
2021-06-09 13:46:53 +0900

    new

--- script/main.js
+++ script/main.js
@@ -25,12 +25,32 @@ $(function () {
     $("aside span").on("click", function () {
         $(this).toggleClass("active");
     })
-$(".tags li").on("click", function(){
-    $(this).toggleClass("active")
-})
-$(".results li").on("click", function(){
-    $(this).addClass("active");
-    $(".results li").not($(this)).removeClass("active")
-})
+    $(".tags li").on("click", function () {
+        $(this).toggleClass("active")
+    })
+    $(".results li").on("click", function () {
+        $(this).addClass("active");
+        $(".results li").not($(this)).removeClass("active")
+    })
+
+    $("p.prev").on("click", function () {
+        if ($("article.all_figs div").position().left <= 0) {
+            $("article.all_figs div").animate({
+                left: "-=80vw"
+            })
+        } else {
+        
+        }
+    })
+    $("p.next").on("click", function () {
+        if ($("article.all_figs div").position().left == 0) {
+            
+        } else {
+            $("article.all_figs div").animate({
+                left: "+=80vw"
+            })
+        }
+    })
+
     //-----jquery end--------
 });
