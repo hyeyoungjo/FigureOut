4cfa0f7a3f88dccc27b010799f6f0eefb7f95236
ykim18 <ykim18@kaist.ac.kr>
2021-06-12 02:57:55 +0900

    ajax_connect

--- script/load.js
+++ script/load.js
@@ -1,41 +1,109 @@
-$.ajax({
-    url: 'data/list.xml',
-    type: 'GET',
-    dataType: 'xml',
-    beforeSend: function () {},
-    complete: function () {},
-    success: function (list) {
-        
-        var $aside = "<aside><span class='thumb'></span><span class='cart'></span></aside>";
+$(function () {
+    //-----jquery start
+    $.ajax({
+        url: 'data/one.xml',
+        type: 'GET',
+        dataType: 'xml',
+        beforeSend: function () {},
+        complete: function () {},
+        success: function (one) {
+            var $articleIdx = 0;
+            var $figList="";
+            var $info;
+            var $caption;
 
-        $(list).find("items").find("article").each(function(){
-            var $title=$(this).find("title").html();
-            var $venue=$(this).find("venue").html();
-            var $paperLength=$(this).find("paperlength").html();
-            var $secLength=$(this)
-            $figures
-        });
-        // $(list).find("items").find("item").each(function () {
-        //     var imgSrc = $(this).find("thumb").html();
-        //     var title = $(this).find("title").html();
-        //     var year = $(this).find("year").html();
-        //     $figureG += "<figure><p><img src='" + imgSrc + "'></p><figcaption><h4>" + title + "</h4></figcaption><small>" + year + "</small></figure>"
-        // });
+            var $paperLength = 0;
+            var $secLength = 0;
 
-        // $(list).find("threeD").find("item").each(function () {
-        //     var imgSrc = $(this).find("thumb").html();
-        //     var title = $(this).find("title").html();
-        //     var year = $(this).find("year").html();
+            var $bar;
 
-        //     $figure3 += "<figure><p><img src='" + imgSrc + "'></p><figcaption><h4>" + title + "</h4></figcaption><small>" + year + "</small></figure>"
-        // });
+            var $fig = "";
 
-        // $(".graphic_con section.thum_container").append($figureG);
-        // $(".threeD_con section.thum_container").append($figure3);
+            var $mainThumb = "<p class='img'></p>";
+            var $aside = "<aside><span class='thumb'></span><span class='cart'></span></aside>";
 
-    },
-    error: function () {
-        alert('Fail');
-    }
-    //ajax end
+            $(one).find("article").each(function () {
+                var $secBar = "";
+                var $figItem = "";
+                $articleIdx = $(this).index();
+                // info
+                var $venue = "<span class='conf'>" + $(this).find("venue").html() + "</span>";
+                var $cite = "<span class='cite'><span></span>" + Math.round(Math.random() * 100 + 1) + "</span>";
+                var $down = "<span class='down'><span></span>" + Math.round(Math.random() * 100 + 1) + "</span>";
+                $info = "<div class='info'>" + $venue + $cite + $down + "</div>"
+
+                // doi
+                var $doi = $(this).find("doi").html();
+
+                // div.caption
+                var $title = "<h3>" + $(this).find("data>title").html() + "</h3>";
+                var $authors = "<ul class='authors'>" + $(this).find("authors").html() + "</ul>";
+                var $abstract = "<span class='abstract'>" + $(this).find("abstract").html() + "</span>";
+                var $figDes = "<p class='fig_des'><span>◄</span><span></span></p>";
+                $caption = "<div class='caption'>" + $title + $authors + $abstract + $figDes + "</div>"
+
+                // sections 
+                // $(this).find("sections").find("section").each(function (i) {
+                //     var $length = Number($(this).find("word_count").html());
+                //     $paperLength += $length;
+                // });
+
+                $(this).find("sections").find("section").each(function (i) {
+
+                    // $secLength = Math.ceil(Number($(this).find("word_count").html()) / $paperLength * 100) + "%";
+                    var $secTitle = $(this).find("title").html();
+                    $secBar += "<span id='" + $secTitle.replace(" ", "_") + "'>" + $secTitle + "</span>"
+                });
+                $bar = "<div class='bar'>" + $secBar + "</div>";
+
+                // figures
+                $(this).find("figure").each(function (i) {
+                    $figId = $(this).find("id").html();
+                    $figUrl = "'crops/" + $(this).find("url").html() + "'";
+                    $figItem += "<img src=" + $figUrl + ">";
+                });
+                $fig = "<div class='figures'>" + $figItem + "</div>";
+
+                // figure lists 
+                $figList += "<div class='card'><figure>" + $mainThumb + "<figcaption>" + $bar + $info + $caption + $fig + "</figcaption></figure>" + $aside + "</div>";
+            });
+
+            $("section#mainContainer").append($figList);
+            $("div.card").each(function () {
+                var thisIdx=$(this).index();
+                var mainImg = $(one).find("article").eq(thisIdx).find("url").html();
+               $(this).find("p.img").css({
+                   backgroundImage: "url('../crops/"+mainImg+"')"
+               })
+            });
+            //figures jquery
+            var imgSrc = "";
+            var imgIdx = 0;
+            $("p.img").on("mouseenter", function () {
+                $(this).siblings("figcaption").find("div.figures").animate({
+                    bottom: "12px"
+                });
+                $(this).siblings("figcaption").find("span.abstract").slideUp(150);
+                $(this).siblings("figcaption").find("p.fig_des").delay(150).animate({
+                    height: "30px"
+                }, 150);
+            });
+            $("figure").on("mouseleave", function () {
+                $(this).find("div.figures").animate({
+                    bottom: "-100px"
+                });
+                $(this).find("span.abstract").slideDown(150);
+                $(this).find("p.fig_des").css({
+                    height: 0
+                });
+            });
+        },
+
+        error: function () {
+            alert('Fail');
+        }
+        //ajax end
+    });
+
+    //---jquery end
 });
--- script/main.js
+++ script/main.js
@@ -1,27 +1,15 @@
 $(function () {
     //-----jquery start------
-    var imgSrc = "";
-    var imgIdx = 0;
+   
+
     $(".figures img").on("mouseenter", function () {
         imgSrc = $(this).attr('src');
         imgIdx = $(this).index() + 1;
         $(this).parents("figure").find("p.img").css("background-image", "url('" + imgSrc + "')")
         $(this).parents("figure").find("p.fig_des span:nth-of-type(2)").html("Figure " + imgIdx + ". Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temp")
     })
-    $(".list p.img").on("mouseenter", function () {
-        $(this).siblings("figcaption").find("div.figures").animate({
-            bottom: "12px"
-        });
-        $(this).siblings("figcaption").find("span.abstract").hide();
-        $(this).siblings("figcaption").find("p.fig_des").delay(200).css("display", "flex");
-    })
-    $("figure").on("mouseleave", function () {
-        $(this).find("div.figures").animate({
-            bottom: "-100px"
-        });
-        $(this).find("span.abstract").delay(100).show();
-        $(this).find("p.fig_des").hide();
-    })
+
+    
     $("aside span").on("click", function () {
         $(this).toggleClass("active");
     })
@@ -39,12 +27,12 @@ $(function () {
                 left: "-=80vw"
             })
         } else {
-        
+
         }
     })
     $("p.prev").on("click", function () {
         if ($("article.all_figs div").position().left == 0) {
-            
+
         } else {
             $("article.all_figs div").animate({
                 left: "+=80vw"
