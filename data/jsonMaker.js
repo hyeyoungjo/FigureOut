a5b9d628474099d2c5a8bd828e2c8261f6b48f6c
ooooooseok@gmail.com <62334707+gh-ooooseok@users.noreply.github.com>
2021-06-14 00:01:44 +0900

    -

---
+++ data/jsonMaker.js
@@ -0,0 +1,50 @@
+var metaData= './x/json/metadata/'; 
+var figData= './x/json/figures/'; 
+const { O_NONBLOCK } = require('constants');
+var fs = require('fs');
+
+var figureListJson = new Array();
+
+fs.readdir(metaData, (err, filelist) => { 
+    console.log(filelist);
+
+    filelist.forEach(file => {
+        if(file.includes('.json')) {
+            var obj = new Object();
+            obj.filename = file.split('_')[0];
+
+            var data = fs.readFileSync( '' + metaData + file, 'utf8');
+            var jsonData=JSON.parse(data);
+            
+            obj.data = jsonData;
+
+            var jsonFig = null;
+            try {
+                var fig = fs.readFileSync('' + figData + file.split('_')[0] + '_figures.json', 'utf8');
+                jsonFig=JSON.parse(fig);
+
+                var figlist = new Array();
+                for (var key in jsonFig.captions) {
+                    // console.log(key + ": " + jsonFig.captions[key]); 
+                    var newfigure = new Object();
+                    newfigure.url = obj.filename + "_crop_"+key+".jpg";
+                    newfigure.type = 'human';
+                    figlist.push(newfigure);
+                } 
+                var temp = new Object();
+                temp.file = obj.filename;
+                temp.figure = figlist;
+                figureListJson.push(temp);
+            } 
+            catch (error) {
+            }            
+        }
+    });
+
+    fs.writeFile("./data/figureType.json",JSON.stringify(figureListJson), function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+})
+
